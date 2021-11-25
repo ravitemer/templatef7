882a644dc255c117309b4c979e27351bd4c854d9
ravitemer <80028678+ravitemer@users.noreply.github.com>
2021-11-25 13:53:08 +0530

    initial commit

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,32 +1,8 @@
 <Page name="home">
-  <!-- Top Navbar -->
-  <Navbar large sliding={false}>
-    <NavLeft>
-      <Link iconIos="f7:menu" iconAurora="f7:menu" iconMd="material:menu" panelOpen="left" />
-    </NavLeft>
-    <NavTitle sliding>Test</NavTitle>
-    <NavRight>
-      <Link iconIos="f7:menu" iconAurora="f7:menu" iconMd="material:menu" panelOpen="right" />
-    </NavRight>
-    <NavTitleLarge>Test</NavTitleLarge>
-  </Navbar>
-  <!-- Toolbar -->
-  <Toolbar bottom>
-    <Link>Left link</Link>
-    <Link>Right Link</Link>
-  </Toolbar>
-  <div class="bg-green-200 w-full overflow-scroll flex ">
-    {#each items as item}
-      <div class="flex-auto flex-shrink-0">
-        <Post {...item}>
 
-        </Post>
-        
-      </div>
-    {:else}
-      No items
-    {/each}
-  </div>
+
+
+
 </Page>
 <script>
   import {
@@ -45,21 +21,5 @@
     Row,
     Col,
     Button
-  } from 'framework7-svelte';
-  import Post from '../components/post.svelte';
-
-  let items = [
-    {
-      App : "Twitter",
-      Text : "this  ish  isdi  his  ishdfh jshjdh jhdjh sd shjfh  sdjhfjh jhjhfjs hjdhjdh fad jfhjshd "
-    },
-    {
-      App : "Twitter",
-      Text : "this  ish  isdi  his  ishdfh jshjdh jhdjh sd shjfh  sdjhfjh jhjhfjs hjdhjdh fad jfhjshd "
-    },
-    {
-      App : "Twitter",
-      Text : "this  ish  isdi  his  ishdfh jshjdh jhdjh sd shjfh  sdjhfjh jhjhfjs hjdhjdh fad jfhjshd "
-    }
-  ]
+  } from "framework7-svelte";
 </script>
